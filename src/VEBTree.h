1d520f6a03e4dcf3b943dbc8522e2dd36b31705b
DLaboratory <dlaboratory@126.com>
2016-08-05 16:04:04 +0800

    Basic member data of a Van Emde Boas tree

--- src/VEBTree.h
+++ src/VEBTree.h
@@ -1,4 +1,16 @@
 #pragma once
 
 class VEBTree
-{};
+{
+    private:
+        unsigned char m_min, m_max;
+        unsigned int m_u;
+        VEBTree *m_summary;
+        VEBTree **m_cluster;
+    public:
+        unsigned int sqrt_u();
+        unsigned int high(unsigned int x);
+        unsigned int low(unsigned int x);
+        unsigned int index(unsigned int x, unsigned int y);
+};
+
